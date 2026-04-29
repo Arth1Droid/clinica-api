5e554edb4c988e03d5ed45e96cb622227d985d9f
ArthDroid1 <barbosarthur7@gmail.com>
2026-04-29 19:01:46 -0300

    feat: create patient entity

---
+++ src/main/java/com/arthdroid1/clinica_api/models/entities/Patient.java
@@ -0,0 +1,101 @@
+package com.arthdroid1.clinica_api.models.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "tb_patient")
+public class Patient implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@Column(name = "name", nullable = false)
+	private String name;
+	@NotBlank
+	@Email(message = "the email field must contain a valid email")
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+	@Pattern(regexp = "\\d{10,11}", message = "Invalid phone number")
+	@Column(name = "telephone", nullable = false)
+	@NotBlank
+	private String telephone;
+	@NotNull
+	@Column(name = "birth_date", nullable = false)
+	private LocalDate birthDate;
+
+	public Patient() {
+	}
+
+	public Patient(String name, String email, String telephone, LocalDate birthDate) {
+		this.name = name;
+		this.email = email;
+		this.telephone = telephone;
+		this.birthDate = birthDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Patient other = (Patient) obj;
+		return Objects.equals(id, other.id);
+	}
+}
