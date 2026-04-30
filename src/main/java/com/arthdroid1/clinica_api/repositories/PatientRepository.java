60233d199563664264bbef2235bc129e7ad45d10
ArthDroid1 <barbosarthur7@gmail.com>
2026-04-29 21:35:14 -0300

    feat: create patient repository

---
+++ src/main/java/com/arthdroid1/clinica_api/repositories/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.arthdroid1.clinica_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arthdroid1.clinica_api.models.entities.Patient;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
